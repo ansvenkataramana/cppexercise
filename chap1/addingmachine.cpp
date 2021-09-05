ea4a5568e56e4b88bd3fad01e0afe39e7c5b942a
Venkataramana ANS <ansvenkataramana@gmail.com>
2021-09-05 21:40:59 +0530

    Updated exercise with additional functions

--- chap1/addingmachine.cpp
+++ chap1/addingmachine.cpp
@@ -3,14 +3,17 @@
 int main()
 {
     int sum = 0, n = 1;
+    int count = 0;
     std::cout << "Enter a list of numbers terminted by a non numeric character: " << std::endl;
 
     while (std::cin >> n)
     {
         sum += n;
+        count++;
     }
 
-    std::cout << "Total is " << sum << std::endl;
+    std::cout << "Count of entered numbers is " << count << " and the Total is " << sum << std::endl;
+    std::cout << "Average of entered numbers is " << sum / (double)count << std::endl;
 
     std::cin.ignore();
     return 0;
